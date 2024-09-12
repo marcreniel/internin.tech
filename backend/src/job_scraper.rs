74111e947962de5e45ddeec064be3e0839bc472e
marcreniel <marcbernardino2005@gmail.com>
2024-09-12 00:38:09 -0700

    refactor to write to json file, more link throughput

--- backend/src/job_scraper.rs
+++ backend/src/job_scraper.rs
@@ -3,27 +3,30 @@ use regex::Regex;
 use scraper::{Html, Selector};
 use crate::constants::rules::{JobRule, JOB_RULES, UUID_PATTERN};
 use url::Url;
+use std::fs::OpenOptions;
+use std::io::prelude::*;
+use serde_json::json;
 
 pub struct JobScraper {
     base_url: String,
     query: String,
     tbs: String,
     client: reqwest::Client,
+    sites: Vec<&'static str>,
 }
 
 impl JobScraper {
-    pub fn new(base_url: &str, query: &str, site_filter: &str, tbs: &str) -> Result<Self, Box<dyn std::error::Error>> {
+    pub fn new(base_url: &str, query: &str, tbs: &str, sites: Vec<&'static str>) -> Result<Self, Box<dyn std::error::Error>> {
         let client = reqwest::Client::builder()
             .user_agent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
             .build()?;
 
-        let full_query = format!("{} site:{}", query, site_filter);
-
         Ok(Self {
             base_url: base_url.to_string(),
-            query: full_query,
+            query: query.to_string(),
             tbs: tbs.to_string(),
             client,
+            sites,
         })
     }
 
@@ -46,50 +49,62 @@ impl JobScraper {
         rule.patterns.is_empty()
     }
 
-    pub async fn scrape(&self) -> Result<Vec<String>, Box<dyn std::error::Error>> {
-        let mut start = 0;
-        let mut job_links: Vec<String> = Vec::new();
-        let mut has_next_page = true;
+    async fn write_link_to_file(&self, href: &str) -> Result<(), Box<dyn std::error::Error>> {
+        std::fs::create_dir_all("output")?;
+        let file_path = "output/links.json";
 
-        while has_next_page {
-            let params = vec![
-                ("q".to_string(), self.query.clone()),
-                ("tbs".to_string(), self.tbs.clone()),
-                ("start".to_string(), start.to_string()),
-            ];
+        let file = OpenOptions::new().append(true).create(true).open(file_path)?;
+        let mut file = std::io::BufWriter::new(file);
 
-            let url = Url::parse_with_params(&self.base_url, &params)?;
-            println!("Fetching page: {}", url);
+        let json_link = json!({ "link": href });
+        writeln!(file, "{}", serde_json::to_string(&json_link)?)?;
+        Ok(())
+    }
 
-            let resp = self.client.get(url).send().await?.text().await?;
-            let document = Html::parse_document(&resp);
-            let link_selector = Selector::parse("a").unwrap();
-            let next_selector = Selector::parse("a#pnnext").unwrap();
+    pub async fn scrape(&self) -> Result<(), Box<dyn std::error::Error>> {
+        for site in &self.sites {
+            let mut start = 0;
+            let mut has_next_page = true;
 
-            let mut job_links_found = 0;
-            for element in document.select(&link_selector) {
-                if let Some(href) = element.value().attr("href") {
-                    if JOB_RULES.iter().any(|rule| Self::matches_rule(href, rule)) {
-                        job_links_found += 1;
-                        job_links.push(href.to_string());
+            while has_next_page {
+                let params = vec![
+                    ("q".to_string(), format!("{} site:{}", self.query, site)),
+                    ("tbs".to_string(), self.tbs.clone()),
+                    ("start".to_string(), start.to_string()),
+                ];
+
+                let url = Url::parse_with_params(&self.base_url, &params)?;
+                println!("Fetching page: {} for site: {}", url, site);
+
+                let resp = self.client.get(url).send().await?.text().await?;
+                let document = Html::parse_document(&resp);
+                let link_selector = Selector::parse("a").unwrap();
+                let next_selector = Selector::parse("a#pnnext").unwrap();
+
+                let mut job_links_found = 0;
+                for element in document.select(&link_selector) {
+                    if let Some(href) = element.value().attr("href") {
+                        if JOB_RULES.iter().any(|rule| rule.site == *site && Self::matches_rule(href, rule)) {
+                            job_links_found += 1;
+                            self.write_link_to_file(href).await?;
+                        }
                     }
                 }
+
+                println!("Found {} job links on this page for site: {}", job_links_found, site);
+
+                let next_elements: Vec<_> = document.select(&next_selector).collect();
+                has_next_page = !next_elements.is_empty();
+                if has_next_page {
+                    start += 10;
+                    println!("Moving to next page...");
+                } else {
+                    println!("No more pages to process for site: {}", site);
+                }
+
+                tokio::time::sleep(tokio::time::Duration::from_secs(10)).await;
             }
-
-            println!("Found {} job links on this page", job_links_found);
-
-            let next_elements: Vec<_> = document.select(&next_selector).collect();
-            has_next_page = !next_elements.is_empty();
-            if has_next_page {
-                start += 10;
-                println!("Moving to next page...");
-            } else {
-                println!("No more pages to process.");
-            }
-
-            tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
         }
-
-        Ok(job_links)
+        Ok(())
     }
 }
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,18 +1,28 @@
-pub mod job_scraper;
-pub mod constants;
+mod job_scraper;
+mod constants;
 
 use job_scraper::JobScraper;
 use tokio;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let scraper = JobScraper::new("https://www.google.com/search", "Software Engineer Intern", "icims.com", "qdr:m")?;
-    let job_links = scraper.scrape().await?;
-    
-    // Print out or process the job links as needed
-    for link in job_links {
-        println!("{}", link);
-    }
+    let sites = vec![
+        "greenhouse.io",
+        "lever.co",
+        "ashbyhq.com",
+        "paylocity.com",
+        "workable.com",
+        "icims.com",
+        "myworkdayjobs.com",
+        "jobvite.com",
+        "breezy.hr",
+        "jobs.smartrecruiters.com/",
+    ];
+
+    let scraper = JobScraper::new("https://www.google.com/search", "Software Engineer Intern", "qdr:d", sites)?;
+    scraper.scrape().await?;
+
+    println!("Scraping completed. Check output/links.json for the results.");
 
     Ok(())
 }
