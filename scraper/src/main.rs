2983f90a21b25056bfd6ed45602a1b600fdc734c
marcreniel <marcbernardino2005@gmail.com>
2024-09-13 15:01:28 -0700

    changed gitignore, added dependancies, modified client

--- scraper/src/main.rs
+++ scraper/src/main.rs
@@ -1,11 +1,16 @@
 mod scraper_client;
+mod supabase_client;
 mod constants;
 
 use tokio;
 use scraper_client::ScraperClient;
+use supabase_client::SupabaseClient;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let supabase_client = SupabaseClient::new()?;
+    supabase_client.ping().await?;
+
     let sites = vec![
         "greenhouse.io",
     ];
--- scraper/src/supabase_client.rs
+++ scraper/src/supabase_client.rs
@@ -1,18 +1,38 @@
 use postgrest::Postgrest;
+use dotenv::dotenv;
 
 pub struct SupabaseClient {
-    base_url: String,
-    api_key: String,
     client: Postgrest,
 }
 
 impl SupabaseClient {
-    pub fn new(base_url: String, api_key: String) -> Self {
-        let client = Postgrest::new(&base_url);
-        SupabaseClient {
-            base_url,
-            api_key,
+    pub fn new() -> Result<Self, Box<dyn std::error::Error>> {
+        dotenv().ok();
+        
+        let public_key = dotenv::var("SUPABASE_JOBS_PUBLIC_KEY")?;
+        let service_key = dotenv::var("SUPABASE_JOBS_SERVICE_KEY")?;
+        let base_url = dotenv::var("SUPABASE_JOBS_URL")?;
+        
+        let client = Postgrest::new(base_url)
+        .insert_header(
+            "apikey",
+            public_key)
+        .insert_header("Authorization", format!("Bearer {}", service_key));
+
+        Ok(Self {
             client,
+        })
+    }
+
+    pub async fn ping(&self) -> Result<(), Box<dyn std::error::Error>> {
+        let response = self.client.from("ping").select("*").execute().await?;
+        
+        if response.status().is_success() {
+            println!("Ping to DB successful!");
+            Ok(())
+        } else {
+            println!("Ping failed with status: {}", response.status());
+            Err("Ping failed".into())
         }
     }
 }
